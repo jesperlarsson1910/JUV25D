01648bb225130669f0a532a17e62e0fcdce41193
Jesper Larsson <jesper.larsson@iths.se>
2025-09-09 18:14:25 +0200

    First half practise 2
    Signed-off-by: Jesper Larsson <jesper.larsson@iths.se>

--- src/main/java/JUV25D/PractiseUnits/Practise2.java
+++ src/main/java/JUV25D/PractiseUnits/Practise2.java
@@ -31,27 +31,85 @@ public class Practise2 {
     }
 
     static void q2(){
-
+        int num = 65536;
+        for(int numH = num; numH>2; numH = numH >> 1){
+            System.out.println(numH);
+        }
     }
 
     static void q3(){
-
+        for(int i = 20; i>2; i--) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 
     static void q4(){
-
+        StringBuilder allString = new StringBuilder();
+        while(true){
+            String input = System.console().readLine("Sträng: ");
+            allString.append(input);
+            if(input.equals(".")||input.isEmpty()){
+                break;
+            }
+            else {
+                System.out.println(allString.toString());
+                allString.append(" ");
+            }
+        }
     }
 
     static void q5(){
+        int num = Integer.parseInt(System.console().readLine("Nummer: "));
+            if(num <= 2){
+                System.out.println("Behöver ett nummer större än 2");
+            }
+            else{
+                while(num>2){
+                    System.out.println(num);
+                    if(num%2==0){
+                        num = num/2;
+                    }
+                    else{
+                        num = (num*3)+1;
+                    }
+                }
+            }
+
 
     }
 
     static void q6(){
+        int hemligtTal = (int) (Math.random()*100);
+        int gissningar = 0;
 
+        while(true){
+            int gissning = Integer.parseInt(System.console().readLine("Gissning: "));
+            gissningar++;
+            if(gissning>hemligtTal){
+                System.out.println("För högt");
+            }
+            else if(gissning < hemligtTal){
+                System.out.println("För lågt");
+            }
+            else{
+                System.out.println("Du gissade rätt tal efter " + gissningar + " försök");
+                break;
+            }
+        }
     }
 
     static void q7(){
-
+        for(int y=0; y<5; y++) {
+            for(int x=0; x<5; x++) {
+                if( x == y )
+                    System.out.println("#");
+                else
+                    System.out.println(".");
+            }
+            System.out.println("");
+        }
     }
 
     static void q8(){
